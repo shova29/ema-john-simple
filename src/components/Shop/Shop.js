c54404ef4d0f1dfc5f72f279a8a9d73f4b30ec2d
shova29 <halima2996@gmail.com>
2022-03-24 02:06:09 +0600

    order summary added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
@@ -6,17 +7,49 @@ import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  console.log(cart);
 
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    console.log(storedCart);
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+      setCart(savedCart);
+      // addedProduct.quantity = storedCart[id];
+      // setCart([...cart, addedProduct]);
+    }
+    /* if (storedCart) {
+      setCart(JSON.parse(storedCart));
+    } */
+  }, [products]);
 
-  const handleAddToCart = (product) => {
-    console.log(product);
-    const newCart = [...cart, product];
+  const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = [];
+    const existsProduct = cart.find(
+      (product) => product.id === selectedProduct.id
+    );
+    if (!existsProduct) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter((product) => product.id !== selectedProduct.id);
+      existsProduct.quantity = existsProduct.quantity + 1;
+      newCart = [...rest, existsProduct];
+    }
     setCart(newCart);
+    addToDb(selectedProduct.id);
     // setCart([...cart, product]); //another way
   };
 
