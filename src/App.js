680a9fdff95879c0d0d98ce6a843e79bc84afc53
shova29 <halima2996@gmail.com>
2022-03-31 20:33:18 +0600

    custom hook added in orders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Inventory from "./components/Inventory/Inventory";
 import Notfound from "./components/Notfound/Notfound";
@@ -15,7 +16,7 @@ function App() {
         <Route path="/shop" element={<Shop></Shop>}></Route>
         <Route path="/orders" element={<Orders></Orders>}></Route>
         <Route path="/inventory" element={<Inventory></Inventory>}></Route>
-        {/* <Route path="/about" element={<About></About>}></Route> */}
+        <Route path="/about" element={<About></About>}></Route>
         <Route path="*" element={<Notfound></Notfound>}></Route>
       </Routes>
     </div>
