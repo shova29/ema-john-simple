250ccc33bbb56c1d4f969b0754c6eb090ff062c6
shova29 <halima2996@gmail.com>
2022-03-31 20:01:49 +0600

    about added

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>We are gentle people</h1>
+      <p>You can know about us dear!</p>
+    </div>
+  );
+};
+
+export default About;
